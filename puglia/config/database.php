f399e00e7cc104b57e1e19ba4015e457b67c4b6b
b.bianco <perez.marco81@gmail.com>
2019-01-24 19:53:35 +0100

    inserita registrazione /api/register/

--- puglia/config/database.php
+++ puglia/config/database.php
@@ -43,7 +43,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '8000'),
             'database' => env('DB_DATABASE', 'pugliaDB'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'toor'),
--- puglia/routes/api.php
+++ puglia/routes/api.php
@@ -20,6 +20,8 @@ Route::post('articles', 'ArticleController@store');
 Route::put('articles/{article}', 'ArticleController@update');
 Route::delete('articles/{article}', 'ArticleController@delete');
 
+Route::post('register', 'Auth\RegisterController@register');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
